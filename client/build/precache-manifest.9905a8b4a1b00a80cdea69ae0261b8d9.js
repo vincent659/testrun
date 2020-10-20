e1112d3d8302de9995588a65ff9d49c1348eeaae
VincentC <vincent65961@gmail.com>
2020-10-20 13:52:10 +1000

    including react build

---
+++ client/build/precache-manifest.9905a8b4a1b00a80cdea69ae0261b8d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4df50bdc8eeceba137fe7ce0caee365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fe04ef0bc4df3ed0dd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d9eaf75af291b029df5b",
+    "url": "/static/js/2.cbbe6b86.chunk.js"
+  },
+  {
+    "revision": "7f01fb7a78c6328cf95a6a5991494193",
+    "url": "/static/js/2.cbbe6b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fe04ef0bc4df3ed0dd",
+    "url": "/static/js/main.012cb5b5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3d7bcb8bdf74ce990ca6f79063883de1",
+    "url": "/static/media/equal.3d7bcb8b.jpg"
+  },
+  {
+    "revision": "b47e1a1d706e12eff2a753f2b65d0db3",
+    "url": "/static/media/rights.b47e1a1d.jpg"
+  }
+]);
